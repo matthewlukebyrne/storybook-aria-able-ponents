8ce56ed7b204a4f8cc9a42292bc172d7b44ff54c
Matt <matthewlukebyrne@gmail.com>
2021-07-28 22:16:59 +0100

    added global styling, more set up to bring in the final components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom';
 import  { PrimaryButton, SecondaryButton, TeritaryButton } from './components/Buttons';
+import { GlobalStyle } from './utils';
+
 
 const App = () => (
     <div>
         <PrimaryButton>Primary</PrimaryButton>
         <SecondaryButton>Secondary</SecondaryButton>
         <TeritaryButton>Teritary</TeritaryButton>
+        <GlobalStyle />
     </div>
 );
 
